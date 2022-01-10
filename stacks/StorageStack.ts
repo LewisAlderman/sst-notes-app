5fadff8b28e5bb583b41a644790a8926a88931ef
Lewis Alderman <lewisalderman95@gmail.com>
2022-01-10 22:05:17 +0000

    Added a storage stack

---
+++ stacks/StorageStack.ts
@@ -0,0 +1,24 @@
+import * as sst from '@serverless-stack/resources'
+
+export default class StorageStack extends sst.Stack {
+	// Public reference to the table
+	// allows us to reference this in other stacks
+	bucket;
+	table;
+
+	constructor(scope: sst.Construct, id: string, props?: sst.StackProps) {
+		super(scope, id, props);
+
+		this.table = new sst.Table(this, 'Notes', {
+			fields: {
+				userId: sst.TableFieldType.STRING,
+				noteId: sst.TableFieldType.STRING,
+			},
+			primaryIndex: {partitionKey: "userId", sortKey: "noteId"},
+		});
+
+		this.bucket = new sst.Bucket(this, "Uploads");
+	}
+	
+	
+}
