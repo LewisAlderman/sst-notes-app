72580f67fe834b781114e852789be54f9ae1b4f0
Lewis Alderman <lewisalderman95@gmail.com>
2022-01-16 20:09:29 +0000

    Testing lambda timeout error

--- src/get.ts
+++ src/get.ts
@@ -1,17 +1,19 @@
 import handler from "./util/handler";
 import dynamoDb from "./util/dynamodb";
 
-export const main = handler(async(evt) => {
+export const main = handler(async(event) => {
 	const params = {
 		TableName: process.env.TABLE_NAME,
 		Key: { 
-			userId: evt.requestContext.authorizer.iam.cognitoIdentity.identityId, 
-			// noteId: evt.pathParameters.id
+			userId: event.requestContext.authorizer.iam.cognitoIdentity.identityId, 
+			noteId: event.pathParameters.id
 		},
 	}
 
 	const result = await dynamoDb.get(params);
 	if (!result.Item) throw new Error("Could not find note");
 
+	await new Promise(res => setTimeout(res, 10_000));
+	
 	return result.Item;
 })
